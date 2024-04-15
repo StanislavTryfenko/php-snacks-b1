3dc07a81756d9f0d88705d17e75d4725c2b6470b
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-04-15 11:59:00 +0200

    first commit

---
+++ Snack 1/index.php
@@ -0,0 +1,27 @@
+<?php
+
+/* Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+ Ogni array avrà una squadra di casa e una squadra ospite,
+  punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+   Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60 */
+
+$partite = [
+    ['casa' => 'Olimpia Milano',
+    'ospite' => 'Cantù',
+    'punteggio' => '55-60',],
+    ['casa' => 'qualcuno',
+    'ospite' => 'bho',
+    'punteggio' => '55-60',],
+    ['casa' => 'non seguo il basket',
+    'ospite' => 'dammi qualche altra squadra',
+    'punteggio' => '55-60',],
+];
+
+var_dump($partite);
+
+foreach ($partite as $partita) {
+    echo $partita['casa'].' - '.$partita['ospite'].' | '.$partita['punteggio'].'<br>';
+};
+?>
